3015b727a5e22ddb9f29153486787637777d72a9
Dat <haruthong@gmail.com>
2021-05-29 14:16:07 -0700

    Setup Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ErrorPage from "./pages/ErrorPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
-	return <div>testing</div>;
+	return (
+		<Router>
+			<NavBar />
+			<Switch>
+				<Route exact path="/">
+					<HomePage />
+				</Route>
+				<Route exact path="/about">
+					<AboutPage />
+				</Route>
+				<Route>
+					<ErrorPage />
+				</Route>
+			</Switch>
+		</Router>
+	);
 };
 
 export default App;
